07f30746e965685703810d91cadd9628aa86319d
fahrixz <fahrixz56@gmail.com>
2023-07-13 10:08:10 +0700

    f

---
+++ edit_info.js
@@ -0,0 +1,27 @@
+const { modul } = require('./module');
+const edit_info = `Di Bawah`// ga usah di otak atik
+
+const urlpepe = `https://saweria.co/FahriXz`// url/link foto
+
+const namaowner = `FahriXz`// nama owner
+const noowner = `6285769727113`// nomor owner
+
+const packname = `FxzStore`// nama pack sticker
+const author = `©Fahri Xz`// nama author stiker
+
+const pending = `1000`// waktu tunggu broadcast
+/**EDIT WAKTU TUNGGU BROADCAST DI ATAS
+CONTOH :
+1000 = 1 DETIK
+2000 = 2 DETIK
+3000 = 3 DETIK
+DAN SETERUS NYA
+
+🚫TIDAK DI SARAN KAN UNTUK DI BAWAH 1 DETIK🚫
+KARENA AKAN MENYEBAB KAN WA TERBLOKIR
+
+BELI SCRIPT NO ENC, CHAT WHATSAPP DI BAWAH
+HARGA : Rp 50.000
+NOMOR WA : 6285769727113
+**/
+module.exports = { edit_info,urlpepe,namaowner,noowner,packname,author,pending }
